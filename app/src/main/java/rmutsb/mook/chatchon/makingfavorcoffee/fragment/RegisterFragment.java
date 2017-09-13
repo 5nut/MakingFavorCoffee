6eeb2af0372fe991a3a16236e7fc3e897431c4e8
chatchon <nutchatchon@gmail.com>
2017-09-13 16:02:40 +0700

    ok2

--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/RegisterFragment.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/RegisterFragment.java
@@ -7,15 +7,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import rmutsb.mook.chatchon.makingfavorcoffee.MainActivity;
 import rmutsb.mook.chatchon.makingfavorcoffee.R;
+import rmutsb.mook.chatchon.makingfavorcoffee.ultility.MyAlertDialog;
 
 /**
  * Created by nutchatchon 13/9/2560.
  */
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
+
+    //Explicit
+    private String nameString, surnameString, idCardString, EmailString, passwordString;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,13 +38,53 @@ public class RegisterFragment extends Fragment{
         //ToolBar Controller
         toolBarController();
 
+        //Save Controller
+        saveController();
+
+
     } //onActivityCreated
+
+    private void saveController() {
+        ImageView imageView = getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Initial View
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText surNameEditText = getView().findViewById(R.id.edtsurName);
+                EditText idCardEditText = getView().findViewById(R.id.edtidCard);
+                EditText eMailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                //Get Value From Edittext
+                nameString = nameEditText.getText().toString().trim();
+                surnameString = nameEditText.getText().toString().trim();
+                idCardString= nameEditText.getText().toString().trim();
+                EmailString = nameEditText.getText().toString().trim();
+                passwordString = nameEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || surnameString.equals("") ||
+                        idCardString.equals("") || EmailString.equals("") ||
+                        passwordString.equals("")){
+                    //Have Space
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.myDialog(getResources().getString(R.string.title_haveSpace),
+                            getResources().getString(R.string.messsage_haveSpace));
+                }
+
+
+            }  //OnClick
+        });
+    }
 
     private void toolBarController() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
-        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +92,6 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
 
 
     }
