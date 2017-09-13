9fa1f833904f717fce75ddc62797a6f06d5dbfb9
chatchon <nutchatchon@gmail.com>
2017-09-13 14:01:24 +0700

    ok2

--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/MainFragment.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/MainFragment.java
@@ -6,9 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import rmutsb.mook.chatchon.makingfavorcoffee.R;
-
 
 /**
  * Created by WIN10-CREATOR on 13/9/2560.
@@ -22,7 +22,29 @@ public class MainFragment extends Fragment{
         return view;
     } //onCreateView
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register Controller
+        registerController();
+
+    } //onActivityCreated
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentFragmentMain, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
 
 
-
+    }
 }//Main Class
--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/RegisterFragment.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/RegisterFragment.java
@@ -3,14 +3,16 @@ package rmutsb.mook.chatchon.makingfavorcoffee.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import rmutsb.mook.chatchon.makingfavorcoffee.MainActivity;
 import rmutsb.mook.chatchon.makingfavorcoffee.R;
 
 /**
- * Created by WIN10-CREATOR on 13/9/2560.
+ * Created by nutchatchon 13/9/2560.
  */
 
 public class RegisterFragment extends Fragment{
@@ -19,5 +21,31 @@ public class RegisterFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
+    } //onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //ToolBar Controller
+        toolBarController();
+
+    } //onActivityCreated
+
+    private void toolBarController() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
     }
 } //Main Class
